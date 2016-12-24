422aa28c2a719b127a36a60578de6018f9c7da80
jessica486424 <jessica486424@gmail.com>
2016-12-25 02:47:32 +0800

    整合UI介面上傳

---
+++ UI/1218/app/src/main/java/info/devexchanges/navvp/alarms/AlarmListAdapter.java
@@ -0,0 +1,8 @@
+package info.devexchanges.navvp.alarms;
+
+/**
+ * Created by user13 on 2016/12/18.
+ */
+
+public class AlarmListAdapter {
+}
---
+++ UI/1218/app/src/main/java/info/devexchanges/navvp/preferences/AlarmPrefer.java
@@ -0,0 +1,8 @@
+package info.devexchanges.navvp.preferences;
+
+/**
+ * Created by user13 on 2016/12/16.
+ */
+
+public class AlarmPrefer {
+}
